ebf3c80634713317617f82c38398185172057440
fxfangxiu <13718264785@163.com>
2017-12-01 01:30:32 +0800

    提交项目

---
+++ com-fangxiu-exec/src/main/java/com/fangxiu/exec/com_fangxiu_exec/App.java
@@ -0,0 +1,13 @@
+package com.fangxiu.exec.com_fangxiu_exec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
